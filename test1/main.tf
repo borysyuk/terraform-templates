60a62edbb9b9b1ec1ab137237ec99c0397e254ea
Ihor Borysyuk <borysyuk.ihor@gmail.com>
2022-04-08 11:05:18 +0300

    Update main.tf

--- test1/main.tf
+++ test1/main.tf
@@ -3,3 +3,18 @@ resource "null_resource" "example" {
     command = "echo test"
   }
 }
+
+resource "random_pet" "cat" {
+}
+
+resource "random_pet" "dog" {
+}
+
+resource "random_pet" "cheetah" {
+}
+
+resource "random_pet" "horse" {
+}
+
+resource "random_pet" "zebra" {
+}
