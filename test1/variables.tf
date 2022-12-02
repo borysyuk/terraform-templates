c518e65e8b3c2eaa42cec1cefccf41a41fab1cd4
Ihor Borysyuk <borysyuk.ihor@gmail.com>
2022-12-02 11:57:57 +0200

    Create variables.tf

---
+++ test1/variables.tf
@@ -0,0 +1,68 @@
+variable "test_var1" {
+  default = "test1"
+}
+variable "test_var2" {
+  default = "test2"
+}
+variable "test_var3" {
+  default = "test3"
+}
+variable "test_var4" {
+  default = "test4"
+}
+variable "test_var5" {
+  default = "test1"
+}
+variable "test_var6" {
+  default = "test1"
+}variable "test_var7" {
+  default = "test1"
+}
+variable "test_var8" {
+  default = "test1"
+}
+variable "test_var9" {
+  default = "test1"
+}
+variable "test_var10" {
+  default = "test1"
+}
+variable "test_var11" {
+  default = "test1"
+}
+variable "test_var12" {
+  default = "test1"
+}
+variable "test_var13" {
+  default = "test1"
+}
+variable "test_var14" {
+  default = "test1"
+}
+variable "test_var15" {
+  default = "test1"
+}
+variable "test_var16" {
+  default = "test1"
+}
+variable "test_var17" {
+  default = "test1"
+}
+variable "test_var18" {
+  default = "test1"
+}
+variable "test_var19" {
+  default = "test1"
+}
+variable "test_var20" {
+  default = "test1"
+}
+variable "test_var21" {
+  default = "test1"
+}
+variable "test_var22" {
+  default = "test1"
+}
+variable "test_var23" {
+  default = "test1"
+}
