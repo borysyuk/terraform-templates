f314f79b4d7fa77f49572e965cb091ce2763d904
Ihor Borysyuk <borysyuk.ihor@gmail.com>
2022-12-02 12:32:11 +0200

    Update variables.tf

--- test1/variables.tf
+++ test1/variables.tf
@@ -66,3 +66,114 @@ variable "test_var22" {
 variable "test_var23" {
   default = "test1"
 }
+
+variable "region" {
+#   default = "us-east-1"
+}
+variable "instance_type" {
+  type = string
+#   default = "t2.nano"  
+}
+variable "network" {
+  type = string
+#   default = "vpc-596aa03e"
+}
+variable "subnet" {
+  type = string
+#   default = "subnet-3d82d101"
+}
+variable "associate_public_ip" {
+  type    = bool
+  default = true
+}
+variable "2" {
+#   default = "us-east-1"
+}
+variable "3" {
+#   default = "us-east-1"
+}
+variable "regiodn" {
+#   default = "us-east-1"
+}
+variable "dfdnbvcxfn" {
+#   default = "us-east-1"
+}
+variable "regfdfnbvcxfdion" {
+#   default = "us-east-1"
+}
+variable "reg34hjgf34ion" {
+#   default = "us-east-1"
+}
+variable "hgf2r" {
+#   default = "us-east-1"
+}
+variable "thgfd3" {
+#   default = "us-east-1"
+}
+variable "regrdszyiodn" {
+#   default = "us-east-1"
+}
+variable "dfdtdssyfn" {
+#   default = "us-east-1"
+}
+variable "regtytyfdffdion" {
+#   default = "us-east-1"
+}
+variable "reg34gftyty34ion" {
+#   default = "us-east-1"
+}
+variable "dfddfrtrfn" {
+#   default = "us-east-1"
+}
+variable "regfdrtffdion" {
+#   default = "us-east-1"
+}
+variable "reg34rtrtr34ion" {
+#   default = "us-east-1"
+}
+variable "2trtr" {
+#   default = "us-east-1"
+}
+variable "tnbn3" {
+#   default = "us-east-1"
+}
+variable "regynfghiodn" {
+#   default = "us-east-1"
+}
+variable "dfdhgjtyfn" {
+#   default = "us-east-1"
+}
+variable "regtynbxgntyfdffdion" {
+#   default = "us-east-1"
+}
+variable "reg34tgbgyty34ion" {
+#   default = "us-east-1"
+}
+variable "dfdfn" {
+#   default = "us-east-1"
+}
+variable "regfdffdion" {
+#   default = "us-east-1"
+}
+variable "regfghb3434ion" {
+#   default = "us-east-1"
+}
+variable "tr6rytyhh2r" {
+#   default = "us-east-1"
+}
+variable "t3rfggttr" {
+#   default = "us-east-1"
+}
+variable "regyiovcbcvdn" {
+#   default = "us-east-1"
+}
+variable "dfdghxcgfhtyfn" {
+#   default = "us-east-1"
+}
+variable "regcxdion" {
+#   default = "us-east-1"
+}
+variable "reg34gfhtyty34ion" {
+#   default = "us-east-1"
+}
+
