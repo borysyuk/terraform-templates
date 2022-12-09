098d8376dcb07869be015c5ccb3ea1a57c6fc36d
Ihor Borysyuk <borysyuk.ihor@gmail.com>
2022-12-09 16:43:19 +0200

    Update main.tf

--- test1/main.tf
+++ test1/main.tf
@@ -18,3 +18,13 @@ resource "random_pet" "horse" {
 
 resource "random_pet" "zebra" {
 }
+
+output "instance_ip_addr1" {
+  value       = "192.168.0.1"
+  description = "The private IP address of the main server instance."
+}
+
+output "instance_ip_addr2" {
+  value       = "192.168.0.2"
+  description = "The private IP2 address of the main server instance."
+}
