1cf970a9e23eead1b76257e455a298514ae25c92
Ihor Borysyuk <borysyuk.ihor@gmail.com>
2023-03-16 10:37:10 +0200

    Create main.tf

---
+++ test2/main.tf
@@ -0,0 +1,42 @@
+resource "null_resource" "example" {
+  provisioner "local-exec" {
+    command = "echo test"
+  }
+}
+
+variable "image_id" {
+  type = string
+  default="222"
+}
+
+variable "password" {
+  type = string
+  sensitive = true
+  default="pass"
+}
+
+resource "random_pet" "cat" {
+}
+
+resource "random_pet" "dog" {
+}
+
+resource "random_pet" "cheetah" {
+}
+
+resource "random_pet" "horse" {
+  count = 10000
+}
+
+resource "random_pet" "zebra" {
+}
+
+output "instance_ip_addr1" {
+  value       = "192.168.0.1"
+  description = "The private IP address of the main server instance."
+}
+
+output "instance_ip_addr2" {
+  value       = "192.168.0.2"
+  description = "The private IP2 address of the main server instance."
+}
