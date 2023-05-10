5100d95279e700b2848312f250fd01625f0ef995
Ihor Borysyuk <borysyuk.ihor@gmail.com>
2023-05-10 17:02:26 +0300

    Update main.tf

--- test_null/main.tf
+++ test_null/main.tf
@@ -16,13 +16,3 @@ output "instance_ip_addr1" {
   value       = "192.168.0.1"
   description = "The private IP address of the main server instance."
 }
-
-output "instance_ip_addr2" {
-  value       = "192.168.0.2"
-  description = "The private IP2 address of the main server instance."
-}
-
-module "module2" {
-	source  = "mainiacp.ihor-24516.testenv.scalr.dev/acc-svrcncgh453bi8g/module2/git"
-	version = "1.0.2"
-}
