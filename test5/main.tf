af156a989577b7a82e41beff01ba51a39f02be8f
Ihor Borysyuk <borysyuk.ihor@gmail.com>
2023-11-22 11:32:14 +0200

    Create main.tf

---
+++ test5/main.tf
@@ -0,0 +1,32 @@
+variable "docker_ports" {
+  type = list(object({
+    internal = number
+    external = number
+    protocol = string
+  }))
+  default = [
+    {
+      internal = 8300
+      external = 8300
+      protocol = "tcp"
+    }
+  ]
+}
+
+resource "null_resource" "check_ip" {
+  triggers = {
+    current_time = timestamp()
+  }
+  provisioner "local-exec" {
+    command = "curl -s https://ifconfig.me/ip > ip.txt"
+  }
+}
+
+data "local_file" "read_ip" {
+  depends_on = [null_resource.check_ip]
+  filename = "./ip.txt"
+}
+
+output "scalr_ip" {
+  value = "Current instance IP is: ${data.local_file.read_ip.content}\nDocker ports information: ${jsonencode(var.docker_ports)}"
+}
