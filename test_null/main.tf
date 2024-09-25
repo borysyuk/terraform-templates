5f8ede51be00f22723c49202e1b1584b182265ba
Ihor Borysyuk <borysyuk.ihor@gmail.com>
2024-09-25 12:50:46 +0300

    Update main.tf

--- test_null/main.tf
+++ test_null/main.tf
@@ -12,7 +12,11 @@ resource "random_pet" "horse" {
   count = 9
 }
 
+resource "random_pet" "cat" {
+  count = 5
+}
+
 output "instance_ip_addr1" {
-  value       = "192.168.0.1"
+  value       = "192.168.0.44"
   description = "The private IP address of the main server instance."
 }
