5d88ddfdbf4d5489857f65c08ce49702c9e1e458
Ihor Borysyuk <borysyuk.ihor@gmail.com>
2025-03-10 13:08:29 +0200

    Update main.tf

--- test1/main.tf
+++ test1/main.tf
@@ -26,7 +26,7 @@ resource "null_resource" "previous" {}
 resource "time_sleep" "wait_30_seconds" {
   depends_on = [null_resource.previous]
 
-  create_duration = "30s"
+  create_duration = "20s"
 }
 
 resource "time_static" "example23" {}
