7cd591b41d3aa5fecd8087dda6d17247b0f69e02
Ihor Borysyuk <borysyuk.ihor@gmail.com>
2025-06-04 18:40:25 +0300

    Update main.tf

--- provider/envs/main.tf
+++ provider/envs/main.tf
@@ -17,6 +17,18 @@ resource "scalr_environment" "test" {
   count = var.env_count
 }
 
+resource "scalr_environment" "test1" {
+  name = "env-from-provider-${count.index}" 
+  account_id = "acc-svrcncgh453bi8g"
+  count = var.env_count
+}
+
+resource "scalr_environment" "test1" {
+  name = "denv-from-provider-${count.index}" 
+  account_id = "acc-svrcncgh453bi8g"
+  count = var.env_count
+}
+
 data "scalr_vcs_provider" "example" {
   name = "ihor"
 }
