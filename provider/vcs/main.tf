e32bff274a3d311f46ba8049cbc4f708817f4d01
Ihor Borysyuk <borysyuk.ihor@gmail.com>
2026-01-06 12:40:33 +0200

    Create main.tf

---
+++ provider/vcs/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    scalr = {
+      source  = "registry.scalr.io/scalr/scalr"
+      version = "3.11.0"
+    }
+  }
+}
+
+resource "scalr_vcs_provider" "vcs" {
+count = var.count_number
+  name     = "vcs_provider-${count.index}"
+  vcs_type = "github"
+  token    = var.github_vcs_token
+}
+
+
+variable "count_number" {
+  sensitive = false
+}
