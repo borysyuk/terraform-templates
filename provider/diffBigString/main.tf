7e3d43b539ed047923d1eff9f444800a10ae2b66
Ihor Borysyuk <borysyuk.ihor@gmail.com>
2026-02-19 11:53:29 +0200

    Create main.tf for Scalr provider configuration
    Add Terraform configuration for Scalr provider with Kubernetes resource.

---
+++ provider/diffBigString/main.tf
@@ -0,0 +1,19 @@
+terraform {
+    required_providers {
+        scalr = {
+            source = "registry.scalr.io/scalr/scalr"
+            version= "3.13.1"
+        }
+    }
+}
+resource "scalr_provider_configuration" "kubernetes" {
+  name       = "k8s"
+  custom {
+    provider_name = "kubernetes"
+    argument {
+      name        = "host"
+      value       = "https://kubernetes.io/flyscan1/hyperspectral/hydrocarbon-detection-quantification/cpp/0.11.0-rc4@sha256:096e5910715111c60af9d804a455471b626c20ded014bb226c7947decd81d07c"
+   #   value       = "https://kubernetes.io/flyscan1/hyperspectral/hydrocarbon-detection-quantification/cpp/argus:0.11.0-rc4@sha256:096e5910715111c60af9d804a455471b626c20ded014bb226c7947decd81d07c"
+      description = "The hostname (in form of URI) of the Kubernetes API."
+    }
+  }
