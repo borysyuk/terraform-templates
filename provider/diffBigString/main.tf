7b8260ca4c586687532c095a480c622e9d3cd9c8
Ihor Borysyuk <borysyuk.ihor@gmail.com>
2026-02-27 11:13:34 +0200

    Modify host value in Kubernetes provider configuration
    Updated the value for the host argument in the custom provider configuration.

--- provider/diffBigString/main.tf
+++ provider/diffBigString/main.tf
@@ -12,8 +12,8 @@ resource "scalr_provider_configuration" "kubernetes" {
     provider_name = "kubernetes"
     argument {
       name        = "host"
-   #   value       = "https://kubernetes.io/flyscan1/hyperspectral/hydrocarbon-detection-quantification/cpp/0.11.0-rc4@sha256:096e5910715111c60af9d804a455471b626c20ded014bb226c7947decd81d07c"
-      value       = "https://kubernetes.io/flyscan1/hyperspectral/hydrocarbon-detection-quantification/cpp/argus:0.11.0-rc4@sha256:096e5910715111c60af9d804a455471b626c20ded014bb226c7947decd81d07c"
+      value       = "https://kubernetes.io/flyscan1/hyperspectral/hydrocarbon-detection-quantification/cpp/0.11.0-rc4@sha256:096e5910715111c60af9d804a455471b626c20ded014bb226c7947decd81d07c"
+   #   value       = "https://kubernetes.io/flyscan1/hyperspectral/hydrocarbon-detection-quantification/cpp/argus:0.11.0-rc4@sha256:096e5910715111c60af9d804a455471b626c20ded014bb226c7947decd81d07c"
       description = "The hostname (in form of URI) of the Kubernetes API."
     }
   }
