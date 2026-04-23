96d148d449234eca729d749689e0c5f6438a36f7
Ihor Borysyuk <borysyuk.ihor@gmail.com>
2026-04-23 16:59:18 +0300

    Uncomment scalr_workspace resource and remove moved/removed blocks

--- provider/opentofu/main.tf
+++ provider/opentofu/main.tf
@@ -19,29 +19,29 @@ resource "scalr_workspace" "dev" {
   environment_id = data.scalr_environment.playground.id
 }
 
-#resource "scalr_workspace" "forget" {
-#  name = "to-forget"
-#  environment_id = data.scalr_environment.playground.id
-#}
+resource "scalr_workspace" "forget" {
+  name = "to-forget"
+  environment_id = data.scalr_environment.playground.id
+}
 
 resource "scalr_workspace" "import" {
   environment_id = data.scalr_environment.playground.id
   name = "test-20"
 }
 
-moved {
-  from = scalr_workspace.development
-  to   = scalr_workspace.dev
-}
+#moved {
+#  from = scalr_workspace.development
+#  to   = scalr_workspace.dev
+#}
 
 import {
   id = "ws-v0p6f6158qicie1t6"
   to = scalr_workspace.import
 }
 
-removed {
-  from = scalr_workspace.forget
-  lifecycle {
-    destroy = false
-  }
-}
+#removed {
+#  from = scalr_workspace.forget
+#  lifecycle {
+#    destroy = false
+#  }
+#}
