a1ce4786249000cbd285b707afa32df17795b495
Ihor Borysyuk <borysyuk.ihor@gmail.com>
2026-06-22 13:14:27 +0300

    Update main.tf

--- provider/vcs/main.tf
+++ provider/vcs/main.tf
@@ -12,7 +12,7 @@ count = var.count_number
   name     = "vcs_provider-${count.index}"
   vcs_type = "github"
   token    = var.github_vcs_token
-  environments = ["env-v0ord4r0sthdi9es5", "env-v0pad8o7cte8roqak", "env-v0pad8o7ej4afdvbd"]
+  environments = ["env-v0ord4r0sthdi9es5", "env-v0pad8o7dj9umke0k", "env-v0pad8o7djs598fef", "env-v0pad8o7djnvegkb7", "env-v0pad8o7dsvlbeu93", "env-v0pad8o7ej4afdvbd"]
 }
 
 
